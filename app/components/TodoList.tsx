1592337865f9f0c66de5c06f2bebd6dfbaca66ec
Hisashi Iwamoto <hisasi622@gmail.com>
2024-08-31 11:48:11 +0900

    chore: Add TodoList component for managing todo items

---
+++ app/components/TodoList.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import TodoItem from "./TodoItem";
+import AddTodoForm from "./AddTodoForm";
+import { Todo } from "../types/todo";
+
+const TodoList: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = (newTodo: Todo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  const editTodo = (id: number, updatedText: string) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, text: updatedText } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold mb-4">Todo List</h1>
+      <AddTodoForm onAddTodo={addTodo} />
+      <ul className="space-y-2">
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onEdit={editTodo}
+            onDelete={deleteTodo}
+            onToggleComplete={toggleComplete}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
