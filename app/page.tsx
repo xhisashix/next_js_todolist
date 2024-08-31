e8de27b9e8e0259ed6b23dc5a8d48d249b1d8142
Hisashi Iwamoto <hisasi622@gmail.com>
2024-08-31 11:48:38 +0900

    chore: Update language attribute to Japanese in RootLayout component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import TodoList from "./components/TodoList";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className="min-h-screen bg-gray-100 p-4">
+      <TodoList />
+    </main>
+  );
 }
